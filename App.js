60203aaf50a2b0fed046452c445fd4c211ad0a68
arumullabhavana <152505541+arumullabhavana@users.noreply.github.com>
2025-03-09 11:33:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GovernmentSchemes from "./pages/GovtSchemes";
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-6xl font-bold"></h1>
+      <GovernmentSchemes />
+    </div>
+  );
+}
+
+export default App;
