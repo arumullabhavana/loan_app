81e0ad58be91622ad6ae3a17b7edbcad32165ba7
arumullabhavana <152505541+arumullabhavana@users.noreply.github.com>
2025-03-09 11:34:40 +0530

    Create govtSchemesModel.js

---
+++ govtSchemesModel.js
@@ -0,0 +1,19 @@
+const schemes = [
+  {
+    id: 1,
+    name: "Startup India Women Entrepreneurship Program",
+    url: "https://www.startupindia.gov.in/content/sih/en/women_entrepreneurs.html"
+  },
+  {
+    id: 2,
+    name: "Mudra Yojana Scheme",
+    url: "https://www.mudra.org.in/"
+  },
+  {
+    id: 3,
+    name: "Annapurna Scheme",
+    url: "https://www.india.gov.in/annapurna-scheme"
+  }
+];
+
+module.exports = schemes;
